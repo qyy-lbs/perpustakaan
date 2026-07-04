4e85c068fe4cac1f550eb4f5dfd81b85511cbce9
qyy-lbs <husnihaqiqi3@gmail.com>
2026-07-05 00:39:52 +0700

    oke gas

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-import app from "../backend/src/server.js";
+let appPromise;
 
-export default app;
+module.exports = async (req, res) => {
+  try {
+    if (!appPromise) {
+      appPromise = import("../backend/src/server.js").then(
+        (module) => module.default
+      );
+    }
+
+    const app = await appPromise;
+
+    return app(req, res);
+  } catch (error) {
+    console.error("API handler error:", error);
+
+    return res.status(500).json({
+      message: "Terjadi kesalahan pada server",
+      error: error.message,
+    });
+  }
+};
